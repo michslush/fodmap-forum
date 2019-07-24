93374e5dcf0a7ed8d8c90276dd6ab41d64d5a92e
Michaela Burns <michaelaburns0@gmail.com>
2019-07-24 14:53:38 -0400

    committing changes before adding feature: searchByCuisine

--- client/components/SingleRestaurant.js
+++ client/components/SingleRestaurant.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import {Card, ListGroup, ListGroupItem} from 'react-bootstrap'
 
 export const SingleRestaurant = props => {
   const {restaurant} = props
 
-  return <div>{restaurant.name}</div>
+  return (
+    <Card style={{width: '18rem'}}>
+      {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
+      <Card.Body>
+        <Card.Title>{restaurant.name}</Card.Title>
+        <Card.Text>{restaurant.cuisine} Cuisine</Card.Text>
+        <Card.Text>
+          {restaurant.address} {restaurant.city}, {restaurant.state}
+        </Card.Text>
+      </Card.Body>
+      <Card.Body>
+        <Card.Header>Comments</Card.Header>
+        <ListGroup className="list-group-flush">
+          {restaurant.comments.length ? (
+            restaurant.comments.map(comment => (
+              <ListGroupItem key={comment.id}>{comment.content}</ListGroupItem>
+            ))
+          ) : (
+            <div>No comments yet!</div>
+          )}
+        </ListGroup>
+      </Card.Body>
+    </Card>
+  )
 }
